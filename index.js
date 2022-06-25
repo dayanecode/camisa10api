80a1b88e5761b1f5a399046274e1e1a497868328
Dayane Ramos <dayanecode@gmail.com>
2022-06-25 19:58:21 -0300

    Criação de Formulário de Cadastro HTML

--- index.js
+++ index.js
@@ -1,21 +1,29 @@
-'use strict';
-
 const express = require("express")
 const server = express()
 const path = require("path");
 
-
 //Conexão com um banco de dados
 const database = require("./database")
+
+
+//static files
+server.use(express.static("public"))
+server.use("/css", express.static(__dirname + "public/style.css"))
+
+
+//obtendo o formulário HTML
+server.get("/", function (request, response) {
+    response.sendFile(__dirname + "/views/index.html")
+})
+
 
 //função que pega o conteúdo da requisição (request body) e transforma de texto em um objeto javascript (json)
 //A partir daí podemos manipular o conteúdo javascript
 server.use(express.json());
 
-
 //INSERT DB
 function CreateAluno(request, response) {
-    database("alunos").
+    database("ALUNOS").
     insert(request.body).
     then(function (data) {
         response.json(data)
@@ -28,8 +36,8 @@ function CreateAluno(request, response) {
 }
 
 //SELECT * DB
-function ListAlunos(request, response) {
-    database("alunos").
+function ListALUNOS(request, response) {
+    database("ALUNOS").
     then(function (data) {
         if (data.lenght == 0) {
             response.status(404)
@@ -47,7 +55,7 @@ function ListAlunos(request, response) {
 
 //Select DB COM WHERE
 function GetAlunoByID(request, response) {
-    database("alunos").
+    database("ALUNOS").
     where('id_aluno', request.params.id).
     then(function (data) {
         response.json(data)
@@ -59,10 +67,10 @@ function GetAlunoByID(request, response) {
 }
 
 //METHODS REQUEST
-server.post("/alunos", CreateAluno)
-server.get("/alunos", ListAlunos)
-server.get("/alunos/:id", GetAlunoByID)
-server.get("/", function (request, response) {
-    res.sendFile(path.join(__dirname, './html/index.html'));
-})
+server.post("/ALUNOS", CreateAluno)
+server.get("/ALUNOS", ListALUNOS)
+server.get("/ALUNOS/:id", GetAlunoByID)
+// server.get("/", function (request, response) {
+//     res.sendFile(path.join(__dirname, './html/index.html'));
+// })
 server.listen(1000)
