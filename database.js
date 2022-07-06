194fb0701356560d70964b9a27deba1388536a3f
Dayane Ramos <dayanecode@gmail.com>
2022-07-06 13:42:23 -0300

    conecta DB com variaveis de ambientes

--- database.js
+++ database.js
@@ -3,9 +3,9 @@ const knex = require("knex")
 module.exports = knex({
     client: 'mysql2',
     connection: {
-        host : process.env.DATABASE_HOST,
-        user : process.env.DATABASE_USER,
-        password : process.env.DATABASE_PASS,
-        database : process.env.DATABASE_NAME
+        host: process.env.HOST,
+        user: process.env.USER,
+        password: process.env.PASS,
+        database: process.env.DATABASE
     }
 })
